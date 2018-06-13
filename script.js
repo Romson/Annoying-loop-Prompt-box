6e315c0d356eabba89b1bf8ba23c9e7f9e2ed12e
romson <romerolijkwan@gmail.com>
2018-06-13 18:22:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+//While loops
+var x = prompt('Are we there yet?');
+
+while (x !== 'yes' && x !== 'yeah') {
+    prompt('Are we there yet?');    
+} alert('YAY, WE MADE IT!');
+*/
+
+//Check .indexOf
+var input = prompt('Are we there yet?');
+
+while (input.indexOf('yes') === -1 && input.indexOf('yeah') === -1) {
+    var input = prompt('Are we there yet?'); 
+}
+alert('YAY, WE MADE IT!');
