14c66fda695b823f21913be0dfa92d4525e6c343
Romero Lijkwan <romerolijkwan@gmail.com>
2018-06-13 18:24:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-/*
 //While loops
 var x = prompt('Are we there yet?');
 
 while (x !== 'yes' && x !== 'yeah') {
     prompt('Are we there yet?');    
 } alert('YAY, WE MADE IT!');
-*/
-
 //Check .indexOf
 var input = prompt('Are we there yet?');
 
